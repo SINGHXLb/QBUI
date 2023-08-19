1c8ba6d205ca65efdbec39925858f3e39b6a322a
Your Name <you@example.com>
2023-08-19 20:54:11 +0800

    added MUI login screen

--- src/AppGoogle.js
+++ src/AppGoogle.js
@@ -1,6 +1,7 @@
 
 import React, {useEffect , useState} from 'react'; 
 import jwt_decode from "jwt-decode" ;
+import Login from './Login'
 
 //import axios from 'axios'; 
  
@@ -9,16 +10,18 @@ function App() {
     
     
 const [ user, setUser ] = useState({}); 
+
  function   handleCallbackResponse(response){
  var userObject = jwt_decode(response.credential);
- console.log(userObject);
+ //console.log(userObject);
  setUser(userObject); 
- document.getElementById("singinDIV").hidden= true;
+//API Call :  Set auth token for application. 
+ //document.getElementById("singinDIV").hidden= true;
  }
 
  function handleSignOut(event ){
     setUser({});
-    document.getElementById("singinDIV").hidden= false;
+   // document.getElementById("singinDIV").hidden= false;
  }
 
 useEffect(()=>{
@@ -31,32 +34,33 @@ google.accounts.id.initialize({
 
 google.accounts.id.renderButton(
     document.getElementById("singinDIV"),
-    {theme:"filled_blue", size:"large"}
+    {theme:"filled_blue"}
 );
 
 //google.accounts.id.prompt();
-} ,[]);
+} ,[user]);
 
-   
     return (
-         
-        <div className='App'>
-        <div id="singinDIV"></div>
-        { Object.keys(user).length !== 0  
-            && <button onClick={(e)=> handleSignOut(e)}>Sign out</button>
-         
-        }
+        <>
+         {Object.keys(user).length === 0   && <Login />}
+        <div className='App'> 
         {
               Object.keys(user).length !== 0  
-              &&  <div>
+              &&  
+              (
+                <>
+                <div>
                   <img src={user.picture} alt="user" /> 
                   <p>Name: {user.name}</p>
                   <p>Email Address: {user.email}</p>
-              </div>
-
+                </div>
+                <button onClick={(e)=> handleSignOut(e)}>Sign out</button>
+                </>
+                )
         } 
       
         </div>
+        </>
     );
 }
 export default App;
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,53 +1,91 @@
 import './Login.css';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 interface LoginInterface {
     handleLogin: React.MouseEventHandler<HTMLButtonElement>
     handleEmailChange: React.ChangeEventHandler<HTMLInputElement> 
-
     //(event: React.MouseEvent<HTMLInputElement, MouseEvent>, index: number) => void
+} 
+ 
+function Copyright(props: any) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
-}
- 
- 
+const defaultTheme = createTheme();
+
+
+
 function Login(prop: LoginInterface ) { 
 
-
-    return (
-
-    
-
-        <Container>
-    
-            
-            <Row>
-                <Col md="3">
-                    <h3>Log In</h3>
-                    <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                        onChange={prop.handleEmailChange}
-                    /> 
-                  
-                </Col> 
-            </Row> 
-            <Row>
-                <Col md="3">
-                    {
-                        <button className="btn btn-primary" onClick={prop.handleLogin} >
-                            Login
-                        </button>
-                    }
-                </Col> 
-            </Row>
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+          email: data.get('email'),
+          password: data.get('password'),
+        });
+      };
 
 
-        </Container>
+
+    return ( 
+ 
+      <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+             <LockOutlinedIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          
+          <Box> 
+
+            <div
+            id="singinDIV" >
+            </div>
+           
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider> 
+
     );
 }
 
